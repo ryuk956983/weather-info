a96c1e7f2c633dccd52e2ce0bf1b4bfb5fd8f2cb
RYUK <c47shmah8@gmail.com>
2024-06-17 20:18:58 +0530

    Your commit message

--- src/Components/Weather/Todaysforecast.jsx
+++ src/Components/Weather/Todaysforecast.jsx
@@ -11,7 +11,7 @@ const Todaysforecast = ({value}) => {
     <div className=' w-24 flex flex-col items-center'>
         <h1 className='text-textGrey font-semibold'>{todayTime} {time[0]>12?"PM":"AM"}</h1>
 
-        <img src={`./src/assets/V1_icons/color/${icon.replace(/-/g,"_")}.svg`} alt="" />
+        <img src={`/assets/V1_icons/color/${icon.replace(/-/g,"_")}.svg`} alt="" />
         <h1 className='text-xl font-semibold'>{todayTemp}<sup>o</sup></h1>
     </div>
   )
